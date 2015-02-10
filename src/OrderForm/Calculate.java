64b4b2cb66f9f0335e85cf12ecb71a53406538a1
Vikash Loomba <loomba.vikash@gmail.com>
2015-02-09 17:46:08 -0800

    initial commit

---
+++ src/OrderForm/Calculate.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package OrderForm;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+
+/**
+ *
+ * @author vikash.loomba
+ */
+public class Calculate {
+    //Instance Variables and constants
+        static final double 
+                whiteBlack = 6.25,
+                redWhite = 5.00,
+                blueBlack = 7.50,
+                TAX = 0.825,
+                DISCOUNT = 0.7;
+        
+    double total = 0.0;
+    
+}
